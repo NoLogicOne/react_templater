a15af57a5543e725383eff989781fd1c03af71eb
Relax <Loginovskikh.K@gmail.com>
2019-05-10 00:47:06 +0300

    build version 1.0

---
+++ build/precache-manifest.2ad6761af7fb1df8f316b1bed3422c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662e97c74b3835146fc1b2a9ab595af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a71d0666c50f88efe5f9",
+    "url": "/static/css/main.c0e443eb.chunk.css"
+  },
+  {
+    "revision": "344ff1414099c668f7dd",
+    "url": "/static/js/2.8ade85cc.chunk.js"
+  },
+  {
+    "revision": "a71d0666c50f88efe5f9",
+    "url": "/static/js/main.6d5b351c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
